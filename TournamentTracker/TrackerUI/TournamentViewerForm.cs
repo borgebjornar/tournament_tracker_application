fff9b64ec3bfd06fecac5c553b6dcb98392117be
Bjørnar Borge <72221999+borgebjornar@users.noreply.github.com>
2022-11-23 09:25:02 +0100

    Create TournamentViewerForm.cs

---
+++ TournamentTracker/TrackerUI/TournamentViewerForm.cs
@@ -0,0 +1,20 @@
+namespace TrackerUI
+{
+    public partial class tournamentViewerForm : Form
+    {
+        public tournamentViewerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
