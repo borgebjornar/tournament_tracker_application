8ec5ecf19cd2c116047990cbc4f3cce9c3b484e3
Bjørnar Borge <72221999+bjornarborge@users.noreply.github.com>
2023-01-02 20:09:42 +0100

    Added database and textfile connectors

--- TournamentTracker/TrackerLibrary/GlobalConfig.cs
+++ TournamentTracker/TrackerLibrary/GlobalConfig.cs
@@ -8,6 +8,23 @@ namespace TrackerLibrary
 {
     public static class GlobalConfig
     {
-        
+        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
+
+        public static void InitalizeConnections(bool database, bool textFiles)
+        {
+            if (database)
+            {
+                // TODO: Set up the SQL Connector properly.
+                SqlConnector sql = new SqlConnector();
+                Connections.Add(sql);
+            }
+            
+            if (textFiles)
+            {
+                // TODO: Set up the Text connections properly.
+                TextConnection text = new TextConnection();
+                Connections.Add(text);
+            }
+        }
     }
 }
