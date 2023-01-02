0fc5bdd2f667cdfa670ed1a17ae9149e4dd5b602
Bjørnar Borge <72221999+bjornarborge@users.noreply.github.com>
2023-01-02 20:09:26 +0100

    Configs

--- TournamentTracker/TrackerUI/Program.cs
+++ TournamentTracker/TrackerUI/Program.cs
@@ -8,10 +8,14 @@ namespace TrackerUI
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new TournamentDashboardForm());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Initialize the database connections.
+            TrackerLibrary.GlobalConfig.InitalizeConnections(true, true);
+            Application.Run(new CreatePrizeForm());
+            
+            //Application.Run(new TournamentDashboardForm());
         }
     }
 }
