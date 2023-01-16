74b57142d4c6929e88e933cb16ed60e1037b5279
Bjørnar Borge <72221999+bjornarborge@users.noreply.github.com>
2023-01-16 19:34:40 +0100

    Update TextConnector.cs

--- TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
+++ TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
@@ -12,6 +12,7 @@ namespace TrackerLibrary.DataAccess
     {
         private const string PrizesFile = "PrizeModels.csv";
         private const string PeopleFile = "PersonModels.csv";
+        private const string TeamFile = "TeamModels.csv";
 
         public PersonModel CreatePerson(PersonModel model)
         {
@@ -32,6 +33,32 @@ namespace TrackerLibrary.DataAccess
             people.SaveToPeopleFile(PeopleFile);
             // Return the new person with the ID.
             return model;
+        }
+
+        public TeamModel CreateTeam(TeamModel model)
+        {
+            List<TeamModel> teams = TeamFile.FullFilePath().LoadFile().ConvertToTeamModels(PeopleFile);
+
+            // Find the ID.
+            int currentId = 1;
+
+            if (teams.Count > 0)
+            {
+                currentId = teams.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
+            // Add the new record with the new ID (max + 1).
+            model.Id = currentId;
+
+            // Convert the prizes to list<string>.
+            teams.Add(model);
+
+            // Save the list<string> to the text file.
+            teams.SaveToTeamFile(TeamFile);
+
+            // Return the new person with the ID.
+            return model;
+
         }
 
         public List<PersonModel> GetPerson_All()
