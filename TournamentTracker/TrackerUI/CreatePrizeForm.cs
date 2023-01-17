1760fdb3f833fbecd59b63dd96173c1eba715407
Bjørnar Borge <72221999+bjornarborge@users.noreply.github.com>
2023-01-17 14:16:33 +0100

    Update CreatePrizeForm.cs

--- TournamentTracker/TrackerUI/CreatePrizeForm.cs
+++ TournamentTracker/TrackerUI/CreatePrizeForm.cs
@@ -15,9 +15,12 @@ namespace TrackerUI
 {
     public partial class CreatePrizeForm : Form
     {
-        public CreatePrizeForm()
+        IPrizeRequester callingForm;
+        public CreatePrizeForm(IPrizeRequester caller)
         {
             InitializeComponent();
+            
+            callingForm = caller;
         }
 
         private void createPrizeButton_Click(object sender, EventArgs e)
@@ -32,11 +35,15 @@ namespace TrackerUI
 
                 GlobalConfig.Connection.CreatePrize(model);
 
+                callingForm.PrizeComplete(model);
+
+                this.Close();
+
                 // Clear the form when the button is pressed.
-                placeNameValue.Text = "";
-                placeNumberValue.Text = "";
-                prizeAmountValue.Text = "0";
-                pricePercentageValue.Text = "0";
+                //placeNameValue.Text = "";
+                //placeNumberValue.Text = "";
+                //prizeAmountValue.Text = "0";
+                //pricePercentageValue.Text = "0";
             }
             else
             {
