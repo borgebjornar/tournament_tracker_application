259c754da697e31d64b2e5e920c348a75fad440b
Bjørnar Borge <72221999+bjornarborge@users.noreply.github.com>
2023-01-19 14:06:52 +0100

    Create Create table - Tournament Entries.sql

---
+++ TournamentTracker/Scripts/Create table - Tournament Entries.sql
@@ -0,0 +1,22 @@
+USE [Tournaments]
+GO
+
+/****** Object:  Table [dbo].[TournamentEntries]    Script Date: 18.01.2023 15:15:22 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[TournamentEntries](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[TournamentId] [int] NOT NULL,
+	[TeamId] [int] NOT NULL,
+ CONSTRAINT [PK_TournamentEntries] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
